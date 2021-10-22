e34c9e7b93efed2ad4b37ee86ecf6e57e8f94188
Yiğit Duman <yigitq@gmail.com>
2021-10-22 15:13:02 +0300

    Change OpenSea link to only show Molten Core

--- types/MoltenCore.ts
+++ types/MoltenCore.ts
@@ -4,7 +4,7 @@ const moltenCore: IProject = {
     thumbnail: "/images/projects/moltencore.png",
     name: "MoltenCore",
     website: "https://ethblock.art/create/40",
-    opensea: "https://opensea.io/collection/blockart",
+    opensea: "https://opensea.io/collection/blockart?search[stringTraits][0][name]=Style&search[stringTraits][0][values][0]=Molten%20Core",
     etherscan: "",
     address: "",
     maxSupply: 1000,
